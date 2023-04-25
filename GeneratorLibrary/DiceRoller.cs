71959cfdfa465f5cde4dcc941a4ee6bcab9f65d8
Antonio Moreno Rubio <amrinformatica10@gmail.com>
2023-04-25 19:42:26 +0200

    Added DiceRoller class

---
+++ GeneratorLibrary/DiceRoller.cs
@@ -0,0 +1,23 @@
+﻿namespace GeneratorLibrary
+{
+    public static class DiceRoller
+    {
+        public static int BasicRoll(int modifiers = 0)
+        {
+            int rollResult = 0;
+            for (int i = 0; i < 3; i++)
+                rollResult += Random.Shared.Next(1, 7);
+
+            return rollResult + modifiers;
+        }
+
+        public static int RollD6(uint diceNumber = 3, int modifiers = 0)
+        {
+            int rollResult = 0;
+            for (int i = 0; i < diceNumber; i++)
+                rollResult += Random.Shared.Next(1, 7);
+
+            return rollResult + modifiers;
+        }
+    }
+}
